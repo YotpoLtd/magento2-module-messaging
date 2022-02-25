0ab5140daaccd54dc83cbb6099215652c9d26f53
manaf <manaf.pm@tryzens.com>
2022-02-25 17:16:12 +0530

    Fix cron scheduler issues and other code review feedback

--- Model/Sync/Reset/Customers.php
+++ Model/Sync/Reset/Customers.php
@@ -6,6 +6,7 @@ class Customers extends \Yotpo\Core\Model\Sync\Reset\Customers
 {
     const CUSTOMERS_SYNC_TABLE = 'yotpo_customers_sync';
     const CRONJOB_CODES = ['yotpo_cron_messaging_customers_sync'];
+    const YOTPO_ENTITY_NAME = 'customer';
 
     /**
      * @return array <string>
@@ -21,5 +22,25 @@ class Customers extends \Yotpo\Core\Model\Sync\Reset\Customers
     protected function getCronJobCodes()
     {
         return self::CRONJOB_CODES;
+    }
+
+    /**
+     * @return string
+     */
+    public function getYotpoEntityName()
+    {
+        return self::YOTPO_ENTITY_NAME;
+    }
+
+    /**
+     * @param int $storeId
+     * @param boolean $skipSyncTables
+     * @return void
+     * @throws \Zend_Db_Statement_Exception
+     */
+    public function resetSync($storeId, $skipSyncTables = false)
+    {
+        parent::resetSync($storeId);
+        $this->setResetInProgressConfig($storeId, '0');
     }
 }
