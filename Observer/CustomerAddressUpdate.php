48ea7141fb6b9d4737f845b7fd3f33d8a496b75b
Daniel Cohen <dcohen@yotpo.com>
2022-03-23 18:23:22 +0200

    feat(Observer\CustomerAddressUpdate):changed customer sync logic to use the new environment variable attribute

--- Observer/CustomerAddressUpdate.php
+++ Observer/CustomerAddressUpdate.php
@@ -7,7 +7,9 @@ use Magento\Framework\Event\Observer;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Customer\Model\Customer;
 use Yotpo\SmsBump\Model\Sync\Customers\Processor as CustomersProcessor;
+use Yotpo\SmsBump\Model\Sync\Customers\Services\CustomersAttributesService;
 use Yotpo\SmsBump\Model\Config;
 use Magento\Framework\App\RequestInterface;
 use Magento\Checkout\Model\Session;
@@ -39,22 +41,30 @@ class CustomerAddressUpdate implements ObserverInterface
     protected $session;
 
     /**
+     * @var CustomersAttributesService
+     */
+    protected $customersAttributesService;
+
+    /**
      * CustomerAddressUpdate constructor.
      * @param CustomersProcessor $customersProcessor
      * @param Config $config
      * @param RequestInterface $request
      * @param Session $session
+     * @param CustomersAttributesService $customersAttributesService
      */
     public function __construct(
         CustomersProcessor $customersProcessor,
         Config $config,
         RequestInterface $request,
-        Session $session
+        Session $session,
+        CustomersAttributesService $customersAttributesService
     ) {
         $this->customersProcessor = $customersProcessor;
         $this->config = $config;
         $this->request = $request;
         $this->session = $session;
+        $this->customersAttributesService = $customersAttributesService;
     }
 
     /**
@@ -75,18 +85,13 @@ class CustomerAddressUpdate implements ObserverInterface
         $isCustomerSyncActive = $this->config->isCustomerSyncActive($customerStoreId);
 
         if (!$this->request->getParam('custSync')) {
-            $this->customersProcessor->resetCustomerSyncStatus(
-                $customer->getId(),
-                $customerStoreId,
-                1,
-                true
-            );
+            $this->customersAttributesService->updateSyncedToYotpoCustomerAttribute($customer, 0);
 
             $customerAddress = $customerAddress->getDefaultBilling() ? $customerAddress : null;
             if ($isCustomerSyncActive && $customerAddress) {
-                    /** @phpstan-ignore-next-line */
-                    $this->request->setParam('custSync', true);//to avoid multiple calls for a single save.
-                    $this->customersProcessor->processCustomer($customer, $customerAddress);
+                /** @phpstan-ignore-next-line */
+                $this->request->setParam('custSync', true);//to avoid multiple calls for a single save.
+                $this->customersProcessor->processCustomer($customer, $customerAddress);
             }
         }
     }
